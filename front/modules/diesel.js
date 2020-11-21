d0249900c229dc6623ea4cfd50f6ea09c358efc6
Herpoel Quentin <herpoel.quentin@gmail.com>
2020-11-21 23:58:20 +0100

    template enabled

--- front/modules/diesel.js
+++ front/modules/diesel.js
@@ -1,10 +1,13 @@
 import * as connexion from './connexion.js';
 import * as format from './format.js';
 import * as chart from './getLineChart.js';
+import * as template from './template.js';
 
 export let fetchDiesel = async (stock) => {
   let data = await connexion.fetchStocks('Gasoil');
-  let diesel = format.formatData(data);
-  console.log(diesel);
+  var diesel = format.formatData(data);
+  console.log(diesel.stockDates);
+  document.getElementById('content').innerHTML = template.getTemplate(diesel);
   chart.getLineChart(diesel.stockDates, diesel.stockLevels);
+
 };
