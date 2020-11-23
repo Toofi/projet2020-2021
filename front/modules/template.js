12c42e117d80dccda33f53820146004ef1bc7b73
Herpoel Quentin <herpoel.quentin@gmail.com>
2020-11-23 17:18:13 +0100

    try catch added on the json parse

--- front/modules/template.js
+++ front/modules/template.js
@@ -1,10 +1,15 @@
-export let getTemplate = (stock) => {
+export let getTemplate = (stockDates, stockLevels) => {
   return `
   <div class="container">
   <div class="chart-container">
     <canvas id="myChart" class="chart"></canvas>
     <h1>Historique</h1>
-    ${stock.stockDates[2]}
+      <table>
+        
+        ${stockDates.map((element) => '<tr><td>'+element+'</td>')}
+        ${stockLevels.map((element) => '<td>'+element+'</td></tr>')}
+        
+      </table>
   </div>
 </div>
 `
