7e0dce10bb32533026afd733bc171bdbaf5dddc1
Herpoel Quentin <herpoel.quentin@gmail.com>
2020-11-24 19:58:46 +0100

    template ok

--- front/main.js
+++ front/main.js
@@ -2,18 +2,13 @@ import * as diesel from './modules/diesel.js';
 
 document.getElementById("diesel").onclick = () => {
   console.log("click on diesel")
-  fetch('./views/diesel.html', { method: 'GET' })
-    .then((response) => {
-      response.text().then((text) => { 
-        diesel.fetchDiesel('gasoil');
-      });
-    });
+  diesel.fetchDiesel('gasoil');
 };
 
-document.getElementById("essence").addEventListener('click', (e) =>{
+document.getElementById("essence").addEventListener('click', (e) => {
   console.log("click on essence");
 });
 
-document.getElementById("adblue").addEventListener('click', (e) =>{
+document.getElementById("adblue").addEventListener('click', (e) => {
   console.log("click on adblue");
 });
